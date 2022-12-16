e3e9b496257fa8bca3e10d7a03af38119d635f58
mehmenmike <mehmenmike@gmail.com>
2022-12-16 16:56:17 +0000

    Move point into library code

--- Day9/day9.go
+++ Day9/day9.go
@@ -44,39 +44,39 @@ func puzzle2(instructions []Instruction) int {
 }
 
 func solve(rope Rope, instructions []Instruction) int {
-	pointsVisitedByTail := make([]Point, 0)
+	pointsVisitedByTail := make([]common.Point, 0)
 
 	for _, instruction := range instructions {
 		for i := 0; i < instruction.distance; i++ {
-			rope.knots[0] = rope.knots[0].translate(instruction.direction)
+			rope.knots[0] = rope.knots[0].Translate(instruction.direction)
 
 			for j := 0; j < len(rope.knots)-1; j++ {
 				head := rope.knots[j]
 				tail := rope.knots[j+1]
 
-				if head.tooFarAway(tail) {
-					var candidates []Point
-					if head.x == tail.x || head.y == tail.y {
-						candidates = []Point{
-							{tail.x + 1, tail.y},
-							{tail.x - 1, tail.y},
-							{tail.x, tail.y + 1},
-							{tail.x, tail.y - 1},
+				if tooFarAway(head, tail) {
+					var candidates []common.Point
+					if head.X == tail.X || head.Y == tail.Y {
+						candidates = []common.Point{
+							{X: tail.X + 1, Y: tail.Y},
+							{X: tail.X - 1, Y: tail.Y},
+							{X: tail.X, Y: tail.Y + 1},
+							{X: tail.X, Y: tail.Y - 1},
 						}
 					} else {
-						candidates = []Point{
-							{tail.x + 1, tail.y + 1},
-							{tail.x + 1, tail.y - 1},
-							{tail.x - 1, tail.y + 1},
-							{tail.x - 1, tail.y - 1},
+						candidates = []common.Point{
+							{X: tail.X + 1, Y: tail.Y + 1},
+							{X: tail.X + 1, Y: tail.Y - 1},
+							{X: tail.X - 1, Y: tail.Y + 1},
+							{X: tail.X - 1, Y: tail.Y - 1},
 						}
 					}
 
-					dist := head.getManhattanDistance(candidates[0])
+					dist := head.GetManhattanDistance(candidates[0])
 					closest := candidates[0]
 					for k := 0; k < len(candidates); k++ {
-						if head.getManhattanDistance(candidates[k]) <= dist {
-							dist = head.getManhattanDistance(candidates[k])
+						if head.GetManhattanDistance(candidates[k]) <= dist {
+							dist = head.GetManhattanDistance(candidates[k])
 							closest = candidates[k]
 						}
 					}
@@ -95,15 +95,15 @@ func solve(rope Rope, instructions []Instruction) int {
 }
 
 type Rope struct {
-	knots []Point
+	knots []common.Point
 }
 
 func newRope(length int) Rope {
 	r := Rope{}
-	r.knots = make([]Point, length)
+	r.knots = make([]common.Point, length)
 
 	for i := 0; i < length; i++ {
-		r.knots[i] = Point{0, 0}
+		r.knots[i] = common.Point{X: 0, Y: 0}
 	}
 
 	return r
@@ -114,15 +114,10 @@ type Instruction struct {
 	distance  int
 }
 
-type Point struct {
-	x int
-	y int
-}
-
-func addIfNew(ps []Point, newPoint Point) []Point {
+func addIfNew(ps []common.Point, newPoint common.Point) []common.Point {
 	seen := false
 	for _, point := range ps {
-		seen = seen || point.equals(newPoint)
+		seen = seen || point.Equals(newPoint)
 	}
 
 	if !seen {
@@ -132,32 +127,9 @@ func addIfNew(ps []Point, newPoint Point) []Point {
 	return ps
 }
 
-func (p1 Point) equals(p2 Point) bool {
-	return p1.x == p2.x && p1.y == p2.y
-}
-
-func (p1 Point) tooFarAway(p2 Point) bool {
-	xDist := int(math.Abs(float64(p1.x - p2.x)))
-	yDist := int(math.Abs(float64(p1.y - p2.y)))
+func tooFarAway(p1 common.Point, p2 common.Point) bool {
+	xDist := int(math.Abs(float64(p1.X - p2.X)))
+	yDist := int(math.Abs(float64(p1.Y - p2.Y)))
 
 	return xDist > 1 || yDist > 1
-}
-
-func (p1 Point) getManhattanDistance(p2 Point) int {
-	return int(math.Abs(float64(p1.x)-float64(p2.x))) + int(math.Abs(float64(p1.y)-float64(p2.y)))
-}
-
-func (p Point) translate(direction string) Point {
-	newP := Point{}
-	switch direction {
-	case "U":
-		newP = Point{p.x, p.y - 1}
-	case "D":
-		newP = Point{p.x, p.y + 1}
-	case "L":
-		newP = Point{p.x - 1, p.y}
-	case "R":
-		newP = Point{p.x + 1, p.y}
-	}
-	return newP
 }
--- common/common.go
+++ common/common.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"math"
 	"os"
 	"strings"
 )
@@ -17,4 +18,32 @@ func ReadFileAsStringArray(path string) []string {
 
 func Stuff(data string) []string {
 	return strings.Split(string(data), "\n")
+}
+
+type Point struct {
+	X int
+	Y int
+}
+
+func (p1 Point) Equals(p2 Point) bool {
+	return p1.X == p2.X && p1.Y == p2.Y
+}
+
+func (p1 Point) GetManhattanDistance(p2 Point) int {
+	return int(math.Abs(float64(p1.X)-float64(p2.X))) + int(math.Abs(float64(p1.Y)-float64(p2.Y)))
+}
+
+func (p Point) Translate(direction string) Point {
+	newP := Point{}
+	switch direction {
+	case "U":
+		newP = Point{X: p.X, Y: p.Y - 1}
+	case "D":
+		newP = Point{X: p.X, Y: p.Y + 1}
+	case "L":
+		newP = Point{X: p.X - 1, Y: p.Y}
+	case "R":
+		newP = Point{X: p.X + 1, Y: p.Y}
+	}
+	return newP
 }
